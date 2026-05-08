b11fa4b0529b675e2a3e1c8598957556e2005607
Raghavendra Sunil <raghav@Raghavendras-MacBook-Air.local>
2026-05-08 21:20:30 +0530

    cors update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // app.use(cors());
 
 app.use(cors({
-  origin:"http://localhost:5173"
+  origin:"https://stately-biscochitos-9e25de.netlify.app/"
 }));
 
 // connectDB();
